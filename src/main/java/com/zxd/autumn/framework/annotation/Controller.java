7532fca75183727ea192863f67f811398f0e229c
zhuxindong <501801307@qq.com>
2018-04-10 22:17:47 +0800

    新增@controller注解

---
+++ src/main/java/com/zxd/autumn/framework/annotation/Controller.java
@@ -0,0 +1,22 @@
+package com.zxd.autumn.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+* @Title: Controller.java  
+* @Package com.zxd.autumn.framework.annotation  
+* @Description: 控制器注解，功能和SpringMVC的@controller注解类似
+* @author zhuxindong  E-mail:501801307@qq.com
+* @date 创建时间：2018年4月10日 下午10:12:03
+* @version 1.0
+*/
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+
+}
