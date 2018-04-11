cace6804e0b43ffec770760f41bedcfebd8db9a4
zhuxindong <501801307@qq.com>
2018-04-11 21:15:18 +0800

    准备开发依赖注入功能

---
+++ src/main/java/com/zxd/autumn/framework/helper/IocHelper.java
@@ -0,0 +1,14 @@
+package com.zxd.autumn.framework.helper;
+
+/**
+* @Title: IocHelper.java  
+* @Package com.zxd.autumn.framework.helper  
+* @Description: 依赖注入助手类，提供依赖注入功能
+* @author zhuxindong  E-mail:501801307@qq.com
+* @date 创建时间：2018年4月11日 下午9:14:47
+* @version 1.0
+*/
+
+public class IocHelper {
+
+}
