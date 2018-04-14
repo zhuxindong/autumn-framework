581d4d0a35259f3d52831ccb6c9a5a532627c71a
zhuxindong <501801307@qq.com>
2018-04-14 15:54:22 +0800

    完成了最重要的请求转发器的开发，至此，框架可以跑起来了

--- src/main/java/com/zxd/autumn/framework/core/DispatcherServlet.java
+++ src/main/java/com/zxd/autumn/framework/core/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package com.zxd.autumn.framework.core;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,11 +16,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import com.zxd.autumn.framework.helper.BeanHelper;
 import com.zxd.autumn.framework.helper.ConfigHelper;
 import com.zxd.autumn.framework.helper.ControllerHelper;
 import com.zxd.autumn.framework.main.InitLoader;
+import com.zxd.autumn.framework.util.CodecUtil;
+import com.zxd.autumn.framework.util.JSONUtil;
+import com.zxd.autumn.framework.util.ReflectionUtil;
+import com.zxd.autumn.framework.util.StreamUtil;
+import com.zxd.autumn.framework.web.Data;
 import com.zxd.autumn.framework.web.Handler;
+import com.zxd.autumn.framework.web.Param;
+import com.zxd.autumn.framework.web.View;
 
 /**
 * @Title: DispatcherServlet.java  
@@ -89,11 +101,104 @@ public class DispatcherServlet extends HttpServlet{
 			}
 			
 			
+			/** -----上面是post过来的参数，下面是通过url以get方式过来的参数----- */
+			String body = CodecUtil.decodeURL(StreamUtil.getString(
+												req.getInputStream()));
+			
+			if (StringUtils.isNotEmpty(body)) {
+				
+				String[] params = StringUtils.split(body, "&");
+				
+				if (ArrayUtils.isNotEmpty(params)) {
+					
+					for (String param : params) {
+						
+						String[] array = StringUtils.split(param, "=");
+						
+						if (ArrayUtils.isNotEmpty(array) && array.length == 2) {
+							
+							String paramName = array[0];
+							String paramValue = array[1];
+							
+							paramMap.put(paramName, paramValue);
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+			
+			
+			
+			/** ---------获取完参数后，封装成Param对象------------ */
+			Param param = new Param(paramMap);
+			
+			/** 调用带有 @RequestMapping 注解的方法 */
+			Method requestMappingMethod = handler.getRequestMappingMethod();
+			Object result = ReflectionUtil.invokeMethod(controllerBean,
+												requestMappingMethod, param);
+			
+			/** 处理返回值 */
+			if (result instanceof View) {
+				
+				/** 返回对应的JSP页面 */
+				View view = (View) result;
+				
+				String path = view.getPath();
+				
+				if (StringUtils.isNotEmpty(path)) {
+					
+					if (path.startsWith("/")) {
+						
+						resp.sendRedirect(req.getContextPath()+path);
+						
+					}else {
+						
+						Map<String, Object> model = view.getModel();
+						
+						for (Map.Entry<String, Object> entry : model.entrySet()) {
+							
+							req.setAttribute(entry.getKey(), entry.getValue());
+							
+						}
+						
+						req.getRequestDispatcher(ConfigHelper.getAppJspPath()+path).forward(req, resp);
+						
+					}
+					
+				}
+				
+				
+			}else if (result instanceof Data) {
+				
+				/** 返回JSON数据 */
+				Data data = (Data) result;
+				
+				Object model = data.getModel();
+				
+				if (model != null) {
+					
+					resp.setContentType("application/json");
+					resp.setCharacterEncoding("UTF-8");
+					
+					PrintWriter writer = resp.getWriter();
+					String json = JSONUtil.toJSON(model);
+					
+					writer.write(json);
+					writer.flush();
+					writer.close();
+					
+				}
+				
+				
+				
+			}
 			
 			
 		}
-		
-		
+			
 	}
 	
 	
